c7a01bf620238270094d1c42f1de50b4b6d258d9
Luis Felipe Palacio Salamanca <luis.palacio@evertecinc.com>
2023-08-24 16:09:16 -0500

    feat: create session service

---
+++ app/Services/Session.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Application\Services;
+
+use Aura\Session\SessionFactory;
+
+class Session
+{
+    protected $session;
+    protected $segment;
+
+    public function __construct()
+    {
+        $this->session = (new SessionFactory())->newInstance($_COOKIE);
+    }
+
+    public function setSegment(string $segment)
+    {
+        $this->segment = $segment;
+    }
+
+    public function getSegment()
+    {
+        return $this->session->getSegment($this->segment);
+    }
+
+    public function get(string $key)
+    {
+        return $this->getSegment()->get($key);
+    }
+
+    public function set(string $key, string $value)
+    {
+        $this->getSegment()->set($key, $value);
+    }
+
+    public function getFlash(string $key)
+    {
+        return $this->getSegment()->getFlash($key);
+    }
+
+    public function setFlash(string $key, string $value)
+    {
+        $this->getSegment()->setFlash($key, $value);
+    }
+
+}
+
