adfb8fce1d3644a67b49afa0703b40465162210e
Luis Felipe Palacio Salamanca <luis.palacio@evertecinc.com>
2023-08-24 15:58:54 -0500

    add request and response for container

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -5,4 +5,13 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotEnv = new \Dotenv\Dotenv(base_path(''));
 $dotEnv->load();
 
-Kint::dump(getenv('DB_NAME'));
+$container = new \League\Container\Container();
+$container->share('response', \Zend\Diactoros\Response::class);
+$container->share('request', function () {
+   return \Zend\Diactoros\ServerRequestFactory::fromGlobals(
+       $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+   );
+});
+
+Kint::dump($container->get('request'));
+
